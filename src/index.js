80d23088f42542786eb4220e2d80b7baae921f5c
Viktoryia_Klimenka <vburchyk@pentaho.com>
2018-03-12 20:26:58 +0300

    sync

--- src/index.js
+++ src/index.js
@@ -1,3 +1,60 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+
+
+  function primes(number) {
+    
+  }
+  
 }
+
+/* let result = 0;
+  let temp = 1;
+  for(let i = 2; i <= number; i++) {
+    temp = temp * gcd( i, base);
+    while(temp % base == 0) {
+      result++;
+      temp /= base;
+    }
+  }
+  console.log("result: " + result);
+  return result;
+
+  function gcd(first, second) {
+    if(first < second) {
+      const temp = first;
+      first = second;
+      second = temp;
+    }
+    let remaining = first % second;
+    if(remaining != 0) {
+      return gcd(second, remaining);
+    } else {
+      return second;
+    }
+  } */
+
+/* let result = 0;
+  let temp = 1;
+  for(let i = 2; i <= number; i++) {
+    temp = gcd(temp * i, base);
+    if(temp % base == 0) {
+      result++;
+      temp /= base;
+    }
+  }
+  console.log("result: " + result);
+  return result;
+
+  function gcd(first, second) {    
+    if(first < second) {
+      const temp = first;
+      first = second;
+      second = temp;
+    }
+    let remaining = first % second;
+    if(remaining != 0) {
+      return gcd(second, remaining);
+    } else {
+      return second;
+    }
+  } */
