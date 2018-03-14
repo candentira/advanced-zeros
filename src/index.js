cdf2363fa5b9c36b0b81e60803c3bf50f20b2478
candentira <vburchyk@pentaho.com>
2018-03-14 12:25:52 +0300

    fix solution

--- src/index.js
+++ src/index.js
@@ -1,36 +1,38 @@
 module.exports = function getZerosCount(number, base) {
   const PRIME_NUMBERS = _getPrimesLess(base);
-  let prPowers = _primesPowers(PRIME_NUMBERS, base);
-  let minPrimePower = Math.min(...prPowers.filter(p => p > 0));
-  let minPrimes = PRIME_NUMBERS
-    .filter((p, i) => prPowers[i] == minPrimePower);
-  let temp = [];//minPrimes.map(() => 0);
-  minPrimes.forEach(p => {
-    let t = 0;
-    let numberCopy = number;
-    while(numberCopy / p >= 1) {
-      t += Math.floor(numberCopy / p);
-      numberCopy /= p;
+  let primes = _primes(PRIME_NUMBERS, base);
+  let temp = primes[0].map(() => 0);
+  primes[0].forEach((p, i) => {
+    for(let numberCopy = number;numberCopy / p >= 1; numberCopy /= p) {
+      temp[i] += Math.floor(numberCopy / p);
     }
-    temp.push(t);
   });
-  let result = Math.min(...temp);
-  console.log("result", result);
-  return result;
+  ;
+  return Math.min(...divideArrays(temp, primes[1]));
 
-  function _primesPowers(primeNumbers, number) {
+  function divideArrays(arr1, arr2) {
+    return arr1.map((a, i) => {
+      return Math.floor(a / (arr2[i] || 1));
+    });
+  }
+
+  function _primes(primeNumbers, number) {
+    let primes = [];
     let powers = [];
     primeNumbers
       .filter(prime => prime <= number)
       .forEach(prime => {
-        let power = 0;
-        while(number % prime == 0) {
-          number /= prime;
-          power++;
+        if(number % prime == 0) {
+          primes.push(prime);
+          let power = 0;
+          while(number % prime == 0) {
+            number /= prime;
+            power++;
+          }
+          powers.push(power);
         }
-        powers.push(power);
       });
-    return powers;
+    return [primes, powers];
   }
 
   function _getPrimesLess(max) {
